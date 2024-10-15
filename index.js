c92477e466abf67e21f414708085e949d98768ef
Charles Caleb <charlesofficial9@gmail.com>
2024-10-15 16:32:20 +0100

    update

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const submitButton = document.getElementById("submitFormBtn");
-const hiddenSubmitButton = document.getElementById("hiddenSubmitButton");
 const signUp_form = document.getElementById("signUp_form");
 submitButton.addEventListener("click", function (e) {
   // e.preventDefault();
 
   const formData = new FormData(signUp_form);
 
-  // Trigger the hidden submit button
-  hiddenSubmitButton.click();
-
-  // for (const [name, value] of formData.entries()) {
-  //   console.log(`${name}: ${value}`);
-  // }
+  for (const [name, value] of formData.entries()) {
+    console.log(`${name}: ${value}`);
+  }
 
   // signUp_form.submit();
 });
